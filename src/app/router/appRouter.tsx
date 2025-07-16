611618c652875284695e9e6276cac80fce604b0c
Konstantin <kostya.svetashov.01@gmail.com>
2025-07-16 13:05:45 +0400

    feat: add CreateTaskPage

--- src/app/router/appRouter.tsx
+++ src/app/router/appRouter.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router";
 import { ROUTES } from "./routes";
 import { HomePage } from "@pages/homepage";
 import { TaskDetailsPage } from "@pages/task-details-page";
+import { CreateTaskPage } from "@pages/create-task-page";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path={ROUTES.HOMEPAGE} element={<HomePage />} />
-      <Route path={ROUTES.NEWTASKPAGE} />
+      <Route path={ROUTES.NEWTASKPAGE} element={<CreateTaskPage />} />
       <Route
         path={ROUTES.TASKDETAILSPAGE + "/:id"}
         element={<TaskDetailsPage />}
